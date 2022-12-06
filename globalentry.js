5a08a95fe43e1b0c36f1a65c0e23914e7693e25b
Jonathan Kart <jkart@lightstep.com>
2022-12-06 15:42:03 -0800

    initial commit

---
+++ globalentry.js
@@ -0,0 +1,92 @@
+import fetch from 'node-fetch';
+import notifier from 'node-notifier';
+import * as R from 'ramda'
+import cliSelect from 'cli-select';
+import prompt from "prompt"
+
+const MINUTE = 60*1000
+
+// kinda trying to avoid obvious scraping...
+// wait for between 4 and 8 minutes (and any millisecond in between)
+// before checking again
+const intervalStart = 4 * MINUTE
+const intervalEnd = 8 * MINUTE
+const maxAppointments = 10
+
+const locationMap = {
+  5446: "SFO" ,
+  5007: "Nogales", //always seem to be appts here.  good for testing the success path
+  // find more location IDs by watching the network calls when you click the location links here:
+  // https://ttp.cbp.dhs.gov/schedulerui/schedule-interview/location?lang=en&vo=true&returnUrl=ttp-external&service=UP
+}
+
+console.log("Where would you like your appt?")
+const selectedLocation = await cliSelect({
+  values: locationMap,
+});
+
+prompt.start();
+const { dateCutoff } = await prompt.get({
+  properties: {
+    dateCutoff: {
+      description: 'Before when? (YYYY-MM-DD)',
+      type: 'string',
+      pattern: /^\d\d\d\d-\d\d-\d\d$/,
+      message: 'Must be in format YYYY-MM-DD',
+      default: '2023-01-01',
+      required: true,
+    }
+  }
+});
+
+console.log(`Checking for appointments as ${selectedLocation.value} before ${dateCutoff}`)
+
+async function checkForAppt(){
+  const response = await fetch(`https://ttp.cbp.dhs.gov/schedulerapi/slots?orderBy=soonest&limit=${maxAppointments}&locationId=${selectedLocation.id}&minimum=1`, {
+    "headers": {
+      "accept": "application/json, text/plain, */*",
+      "accept-language": "en-US,en;q=0.9",
+      "authorization": "",
+      "sec-ch-ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "Referer": "https://ttp.cbp.dhs.gov/schedulerui/schedule-interview/location?lang=en&vo=true&returnUrl=ttp-external&service=UP",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": null,
+    "method": "GET"
+  });
+
+
+  const appointments = await response.json();
+
+  const apptTimes = R.pipe(
+    R.filter(R.propSatisfies(R.gte(dateCutoff),'startTimestamp')),
+    R.pluck("startTimestamp"),
+    R.sortBy(R.identity),
+    R.ifElse(R.isEmpty, () => console.log("None (checked at: ", new Date(), ")"), notifyAppts)
+  )(appointments)
+}
+
+function notifyAppts(apptTimes) {
+  const notification = {
+    title: `${apptTimes.length} appt[s] found at ${selectedLocation.value}!`,
+    message: R.join("\n", apptTimes),
+  }
+  notifier.notify(notification);
+  console.log("Holy shit!", notification)
+}
+
+
+while(true) {
+  checkForAppt();
+  await new Promise(r => setTimeout(r, randomIntFromInterval(intervalStart, intervalEnd)));
+}
+
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
